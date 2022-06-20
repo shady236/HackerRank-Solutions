33e22d0b6df0465a069aa7164f92ea21fc6564c8
shady236 <71782873+shady236@users.noreply.github.com>
2022-06-20 01:37:29 +0000

    Uploaded from HackerHub

---
+++ Java/Java Generics/Java Generics.java
@@ -0,0 +1,12 @@
+
+
+class Printer
+{
+   //Write your code here
+    public <Type> void printArray(Type[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+            System.out.println(arr[i]);
+    }
+}
+
