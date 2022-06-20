19794d35a0227b4fdad34a0d26a0fd0f1ce9024c
shady236 <71782873+shady236@users.noreply.github.com>
2022-06-20 02:59:08 +0000

    Uploaded from HackerHub

---
+++ Algorithms/Sherlock and Cost/Sherlock and Cost.c
@@ -0,0 +1,194 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+int max(int a, int b)
+{
+    if(a > b)       return a;
+    return b;
+}
+
+/*
+ * Complete the 'cost' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY B as parameter.
+ */
+
+int cost(int B_count, int* B)
+{
+    int dp[100001][2] = {0};
+    dp[B_count - 2][0] = B[B_count - 1] - 1;
+    dp[B_count - 2][1] = B[B_count - 2] - 1;
+    for(int i=B_count-3; i>=0; i--)
+    {
+        dp[i][0] = max(B[i + 1] - 1 + dp[i+1][1], dp[i+1][1]);
+        dp[i][1] = max(B[i] - 1 + dp[i+1][0], abs(B[i] - B[i+1]) + dp[i+1][1]);
+    }
+    
+    if(dp[0][0] > dp[0][1])      return dp[0][0];
+    return dp[0][1];
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int t = parse_int(ltrim(rtrim(readline())));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n = parse_int(ltrim(rtrim(readline())));
+
+        char** B_temp = split_string(rtrim(readline()));
+
+        int* B = malloc(n * sizeof(int));
+
+        for (int i = 0; i < n; i++) {
+            int B_item = parse_int(*(B_temp + i));
+
+            *(B + i) = B_item;
+        }
+
+        int result = cost(n, B);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
