1c276a71dfe4238d156b7358990bc4686456e0fc
shady236 <71782873+shady236@users.noreply.github.com>
2022-06-20 01:37:11 +0000

    Uploaded from HackerHub

---
+++ Java/Java BigInteger/Java BigInteger.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        BigInteger a = new BigInteger(scan.nextLine());
+        BigInteger b = new BigInteger(scan.nextLine());
+        scan.close();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+        
+    }
+}
